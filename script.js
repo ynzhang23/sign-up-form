17a14498667b6ad45c1305009fa500d84ec3ae64
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-01-18 22:30:49 +0800

    Add custom validation for password and phone number

---
+++ script.js
@@ -0,0 +1,35 @@
+let passwordInput = document.querySelector('#password');
+let confirmInput = document.querySelector('#confirm-password')
+let input = document.querySelectorAll('input');
+let passwordMessage = document.querySelector('#password-message');
+let passwordValue;
+let confirmValue;
+
+passwordInput.addEventListener('keyup', toggleReadOnly);
+confirmInput.addEventListener('keyup', toggleMessage);
+passwordInput.addEventListener('keyup', toggleMessage);
+
+// Enable user to re-enter password when password is valid
+function toggleReadOnly() {
+  passwordValue = document.querySelector('#password').value;
+  if (passwordValue.length >= 8) {
+    confirmInput.removeAttribute('readonly');
+  } else {
+    confirmInput.value = '';
+    confirmInput.setAttribute('readonly', '');
+  }
+};
+
+// Toggle password match message
+function toggleMessage() {
+  passwordValue = document.querySelector('#password').value;
+  confirmValue = document.querySelector('#confirm-password').value;
+  if (passwordValue == confirmValue && passwordValue.length > 0) {
+    passwordMessage.setAttribute('hidden', '');
+    confirmInput.classList.remove('unmatched');
+  } else if (passwordValue != confirmValue) {
+    passwordMessage.removeAttribute('hidden', '');
+    confirmInput.classList.add('unmatched');
+    confirmInput.setCustomValidity("Invalid field.");
+  }
+}
